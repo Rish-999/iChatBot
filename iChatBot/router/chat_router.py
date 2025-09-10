d606ff1209e34d47381b5f378b01ec208f1b454b
Rishabh Diwakar <111877831+Rish-999@users.noreply.github.com>
2025-09-10 14:22:25 +0530

    Update chat_router.py

--- iChatBot/router/chat_router.py
+++ iChatBot/router/chat_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from model.request_models import QueryRequest
 from services import chat_service
+from iChatBot.model.request_models import QueryRequest
 
 
 router = APIRouter(prefix="/chat",tags=["chat"])
@@ -12,4 +13,5 @@ def start_chat_flow():return chat_service.get_start()
 @router.get("/")
 def root():return  chat_service.root()
 @router.post("/respond")
+
 def respond_chat_flow(req:QueryRequest): return  chat_service.get_response(req.query)
