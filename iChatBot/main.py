c129cbb964452b4b204b69e5c45ded59071a6c4b
Rishabh Diwakar <111877831+Rish-999@users.noreply.github.com>
2025-09-10 16:20:47 +0530

    Update main.py

--- iChatBot/main.py
+++ iChatBot/main.py
@@ -1,18 +1,21 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.staticfiles import StaticFiles
 
 from iChatBot.router.chat_router import router as chat_router
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 app = FastAPI(title="iChatBot")
 
 # Mount the static folder
-# app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount(
     "/static",
     StaticFiles(directory=os.path.join(BASE_DIR, "static")),
     name="static"
 )
+
 # ✅ Allow frontend to talk with backend
 origins = [
     "http://localhost:52221",  # Flutter web dev server
@@ -23,15 +26,11 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # or ["*"] for dev
+    allow_origins=["*"],  # or set origins properly in prod
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-#include routers
-app.include_router(chat_router.router)
-
-
-
-
+# include routers
+app.include_router(chat_router)
